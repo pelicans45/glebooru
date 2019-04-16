70b19fc8234fe7e1a7a354e21d482173f302dbc0
Hunternif <hunternif@gmail.com>
2019-04-17 00:41:43 +0700

    server: fix privilege name for editing metrics

--- server/szurubooru/api/tag_api.py
+++ server/szurubooru/api/tag_api.py
@@ -91,7 +91,7 @@ def update_tag(ctx: rest.Context, params: Dict[str, str]) -> rest.Response:
         _create_if_needed(implications, ctx.user)
         tags.update_tag_implications(tag, implications)
     if ctx.has_param('metric'):
-        auth.verify_privilege(ctx.user, 'metrics:edit')
+        auth.verify_privilege(ctx.user, 'metrics:edit:bounds')
         new_metric = metrics.update_metric(tag, ctx.get_param_as_list('metric')[0])
         if new_metric is not None:
             auth.verify_privilege(ctx.user, 'metrics:create')
