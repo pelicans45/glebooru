def305c11c9f3a8e0e347dab0b2ec03e7275c93c
Hunternif <hunternif@gmail.com>
2019-04-17 00:24:34 +0700

    Update metrics via tag API

--- server/szurubooru/api/metric_api.py
+++ server/szurubooru/api/metric_api.py
@@ -1,6 +1,6 @@
 from typing import Optional, List, Dict
 from szurubooru import db, model, search, rest
-from szurubooru.func import auth, metrics, snapshots, serialization, versions
+from szurubooru.func import auth, metrics, snapshots, serialization, tags
 
 
 def _serialize(ctx: rest.Context, metric: model.Metric) -> rest.Response:
@@ -12,11 +12,12 @@ def _serialize(ctx: rest.Context, metric: model.Metric) -> rest.Response:
 @rest.routes.post('/metrics/?')
 def create_tag(ctx: rest.Context, params: Dict[str, str] = {}) -> rest.Response:
     auth.verify_privilege(ctx.user, 'metrics:create')
-    tag_name = ctx.get_param_as_string_list('tag_name')
-    min = ctx.get_param_as_string_list('min', default=0.)
-    max = ctx.get_param_as_string_list('max', default=10.)
+    tag_name = ctx.get_param_as_string('tag_name')
+    tag = tags.get_tag_by_name(tag_name)
+    min = ctx.get_param_as_float('min', default=0.)
+    max = ctx.get_param_as_float('max', default=10.)
 
-    metric = metrics.create_metric(tag_name, min, max)
+    metric = metrics.create_metric(tag, min, max)
     ctx.session.add(metric)
     ctx.session.flush()
     snapshots.create(metric, ctx.user)
--- server/szurubooru/func/metrics.py
+++ server/szurubooru/func/metrics.py
@@ -1,10 +1,14 @@
 from typing import Any, Optional, Tuple, List, Dict, Callable
 import sqlalchemy as sa
 from szurubooru import config, db, model, errors, rest
-from szurubooru.func import util, serialization, tags
+from szurubooru.func import serialization
 
 
 class MetricAlreadyExistsError(errors.ValidationError):
+    pass
+
+
+class InvalidMetricError(errors.ValidationError):
     pass
 
 
@@ -31,13 +35,26 @@ def serialize_metric(metric: model.Metric, options: List[str] = []) -> Optional[
     return MetricSeralizer(metric).serialize(options)
 
 
-
 def create_metric(
-        tag_name: str,
+        tag: model.Tag,
         min: float,
         max: float) -> model.Metric:
-    tag = tags.get_tag_by_name(tag_name)
     if tag.metric is not None:
         raise MetricAlreadyExistsError('Tag already has a metric.')
     metric = model.Metric(tag=tag, min=min, max=max)
     return metric
+
+
+def update_metric(tag: model.Tag, metric_data) -> Optional[model.Metric]:
+    for field in ('min', 'max'):
+        if field not in metric_data:
+            raise InvalidMetricError('Metric is missing %r field.' % field)
+
+    min, max = metric_data['min'], metric_data['max']
+    if tag.metric is None:
+        tag.metric.min = min
+        tag.metric.max = max
+        return None
+    else:
+        metric = model.Metric(tag=tag, min=min, max=max)
+        return metric
