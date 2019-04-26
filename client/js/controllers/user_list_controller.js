a4b877c0219994536ab7d4c1783d065f9154f70e
Hunternif <hunternif@gmail.com>
2019-04-27 00:45:29 +0700

    client: fix typo

--- client/js/controllers/user_list_controller.js
+++ client/js/controllers/user_list_controller.js
@@ -51,7 +51,7 @@ class UserListController {
             defaultLimit: 30,
             getClientUrlForPage: (offset, limit) => {
                 const parameters = Object.assign(
-                    {}, this._ctx.parameters, {offset, offset, limit: limit});
+                    {}, this._ctx.parameters, {offset: offset, limit: limit});
                 return uri.formatClientLink('users', parameters);
             },
             requestPage: (offset, limit) => {
