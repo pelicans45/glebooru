b83900e273e9a5b0c77d5deaf5962ddb0a70aff6
pardev <pardev@localhost>
2022-12-31 04:04:29 -0500

    Update

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -7,4 +7,4 @@ cd /opt/app
 alembic upgrade head
 
 echo "Starting szurubooru API on port ${PORT} - Running on ${THREADS} threads"
-exec waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
+exec waitress-serve --port ${PORT} --threads ${THREADS} szurubooru.facade:app
