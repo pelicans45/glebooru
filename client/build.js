bda7288ee79ee21bf8e58499cb032ad8fbc88db8
pardev <pardev@localhost>
2023-01-01 02:43:13 -0500

    Update

--- client/build.js
+++ client/build.js
@@ -31,7 +31,7 @@ const external_js = [
 ];
 
 const app_manifest = {
-    name: "szurubooru",
+    name: "Glegle Gallery",
     icons: [
         {
             src: baseUrl() + "img/android-chrome-192x192.png",
