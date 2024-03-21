b581ac4b6982f8a51a5bbfe57631bcf4fcafee46
Eva <evauwu@riseup.net>
2024-03-21 23:05:33 +0100

    server/api: add oEmbed and Open Graph

--- server/szurubooru/api/__init__.py
+++ server/szurubooru/api/__init__.py
@@ -1,6 +1,7 @@
 import szurubooru.api.comment_api
 import szurubooru.api.info_api
 import szurubooru.api.metric_api
+import szurubooru.api.oembed_api
 import szurubooru.api.password_reset_api
 import szurubooru.api.pool_api
 import szurubooru.api.pool_category_api
